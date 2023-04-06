6d20cff3a2a1a7a3f3522e97b26249b886e52896
Alex <61361872+ParPanSs@users.noreply.github.com>
2023-04-06 14:08:34 +0200

    Fixed animations

--- MentalHospital/Assets/Scripts/ButtonTextChanging.cs
+++ MentalHospital/Assets/Scripts/ButtonTextChanging.cs
@@ -1,25 +1,27 @@
+using System;
 using TMPro;
 using UnityEngine;
 
 public class ButtonTextChanging : MonoBehaviour
 {
     public TMP_Dropdown dropdown;
+    public LocalizationPrefs localization;
+
+    public void Start()
+    {
+        PlayerPrefs.DeleteAll();
+    }
 
     public void Update()
     {
         switch (dropdown.captionText.text)
         {
             case "English":
-                PlayerPrefs.SetString("GameLanguage", "en");
-                PlayerPrefs.GetString("GameLanguage");
                 break;
             case "Czech":
                 PlayerPrefs.SetString("GameLanguage", "cs");
-                PlayerPrefs.GetString("GameLanguage");
                 break;
             case "Russian":
-                PlayerPrefs.SetString("GameLanguage", "ru");
-                PlayerPrefs.GetString("GameLanguage");
                 break;
         }
         Debug.Log(PlayerPrefs.GetString("GameLanguage"));
